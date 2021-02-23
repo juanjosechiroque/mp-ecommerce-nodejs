76708c67c2071c5a0d9dded4a3669f7998f7df74
juanjosechiroque <juanjosechiroque@gmail.com>
2021-02-22 23:45:13 -0500

    add integration

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
-var express = require('express');
-var exphbs  = require('express-handlebars');
-var port = process.env.PORT || 3000
+const express = require('express');
+const exphbs  = require('express-handlebars');
 
-var app = express();
+const mercadopago = require ('mercadopago');
+mercadopago.configure({
+  access_token: 'APP_USR-8208253118659647-112521-dd670f3fd6aa9147df51117701a2082e-677408439',
+  integrator_id: 'dev_2e4ad5dd362f11eb809d0242ac130004'
+});
+
+const port = process.env.PORT || 3000
+
+const app = express();
  
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
 app.use(express.static('assets'));
- 
 app.use('/assets', express.static(__dirname + '/assets'));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
 
 app.get('/', function (req, res) {
     res.render('home');
@@ -17,6 +26,91 @@ app.get('/', function (req, res) {
 
 app.get('/detail', function (req, res) {
     res.render('detail', req.query);
+});
+
+app.post('/detail', (req, res) => {
+  
+  const productImage = `${req.protocol}://${req.hostname}${req.body.productImage}`;  
+  const productPrice = +req.body.productPrice;
+    
+  const preference = {
+    items: [
+      {
+        title: req.body.productDescription,
+        quantity: 1,
+        currency_id: 'PEN',
+        picture_url: productImage, 
+        unit_price: productPrice
+      }
+    ],
+    payer: {
+      name: "Lalo",
+      surname: "Landa",
+      email: "test_user_46542185@testuser.com",
+      phone: {
+        area_code: "52",
+        numer: "5549737300"        
+      },
+      identification: {
+        type: "DNI",
+        number: "22334445"
+      },
+      address: {        
+        zip_code: "03940",
+        street_name: "Insurgentes Sur",
+        street_number: 1602,        
+      }
+    },
+    payment_methods: {
+      excluded_payment_types: [
+        { id: "atm" }
+      ],
+      excluded_payment_methods: [        
+        { id: "diners" }
+      ],
+      installments: 6
+    },
+    back_urls: {
+      success: `${req.protocol}://${req.hostname}/responsePayment`,
+      pending: `${req.protocol}://${req.hostname}/responsePayment`,
+      failure: `${req.protocol}://${req.hostname}/responsePayment`,
+    },    
+    notification_url: `${req.protocol}://${req.hostname}/notification`,
+    auto_return: 'approved',
+    external_reference: 'juanjosechiroque@gmail.com'
+  };
+  
+  mercadopago.preferences.create(preference)
+    .then(response => {
+      
+      console.log(response);
+      res.redirect(response.body.init_point);
+    
+    })
+    .catch(err => console.log(err));
+   
+});
+
+app.get('/responsePayment', (req, res) => {
+  
+  if(req.query.collection_status == "approved") {
+    req.query.isSuccess = true;
+  }
+  
+  console.log('req.query');
+  console.log(req.query);
+  
+  res.render('responsePayment', req.query);
+  
+});
+
+app.post('/notification', (req, res) => {
+  
+  console.log(' =========================================================== ');      
+  console.log(req.body);    
+  console.log(' =========================================================== ');      
+  res.status(201);
+  
 });
 
 app.listen(port);
